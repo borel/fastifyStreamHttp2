99d933a61975193f1d0c15d9306a652aa9b7cebb
pbborel <pbborel@gmail.com>
2018-04-13 17:00:23 +0200

    Refactor

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const Fastify = require('fastify');
 const fs = require('fs')
 const path = require('path')
 
-const getKeySomehow = () => {
+const getKey = () => {
  return fs.readFileSync(path.join(__dirname, './resources/localhost.key'));
 }
 
-const getCertSomehow = () => {
+const getCert= () => {
  return fs.readFileSync(path.join(__dirname, './resources/localhost.crt'));
 }
-// https is necessary otherwise browsers will not
-// be able to connect
+
+
 const fastify = Fastify({
  http2: true,
  https: {
-   key: getKeySomehow(),
-   cert: getCertSomehow()
+   key: getKey(),
+   cert: getCert()
  }
 });
 
